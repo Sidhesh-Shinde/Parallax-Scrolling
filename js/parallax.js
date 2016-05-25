5bb9d8061185bcd6a5ee99c23838a284afbb6300
SiD <shinde.sidhesh@gmail.com>
2016-05-25 12:23:57 +0530

    adding comments part 2

--- js/parallax.js
+++ js/parallax.js
@@ -6,5 +6,5 @@ $(function(){
 
 function parallax(){
 	var scrollPosition = $(window).scrollTop();//scrollTop() to check what the scroll position is
-	$('#grid').css('top',(0 - (scrollPosition * .4))+'px');
+	$('#grid').css('top',(0 - (scrollPosition * .4))+'px');//giving the top value to the css property to get parallax effect
 }
